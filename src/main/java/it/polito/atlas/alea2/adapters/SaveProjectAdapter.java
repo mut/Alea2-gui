ba7c1eed62a09305953db7c9f6b552750fd55ec9
DANGELOA <DANGELOA@itbtown500009.avl01.avlcorp.lan>
2011-10-17 21:41:58 +0200

    Add overwrite check on saving

--- src/main/java/it/polito/atlas/alea2/adapters/SaveProjectAdapter.java
+++ src/main/java/it/polito/atlas/alea2/adapters/SaveProjectAdapter.java
@@ -4,8 +4,10 @@ import it.polito.atlas.alea2.Project;
 import it.polito.atlas.alea2.Storage;
 import it.polito.atlas.alea2.components.MainWindowShell;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.MessageBox;
 
 public class SaveProjectAdapter extends SelectionAdapter {
 
@@ -13,8 +15,20 @@ public class SaveProjectAdapter extends SelectionAdapter {
 	public void widgetSelected(SelectionEvent e) {
 		Storage st=MainWindowShell.getStorage();
 		Project p=MainWindowShell.getCurrentProject();
+		if (st.containsProject(p.getName())) {
+			MessageBox mb = new MessageBox(MainWindowShell.shell(), SWT.ICON_WARNING | SWT.YES | SWT.NO);
+			mb.setText("Save on Storage");
+			mb.setMessage("The prject already exist in current storage, would you overwrite?");
+			int rc = mb.open();
+		    switch (rc) {
+		    	case SWT.YES:
+		    		break;
+		    	case SWT.NO:
+		    		return;
+		    }
+		}
 		if  ((p!=null) && (st!=null))
-			st.writeProject(p);
+			st.writeProject(p, true);
 	}
 
 }
